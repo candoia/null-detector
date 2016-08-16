b2f232bbd97321ef7499c59b6252bfe8b1c7a32b
nmtiwari <nmtiwari@iastate.edu>
2016-08-16 00:11:08 -0700

    Changing the app to add bug values. Now the app checks bugs and finds if there are bug fixing revisions. Moreover app now display total bugs, bug fixing revisions and nullfixing revisions in bar chart

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-'use strict';	
-$('#content').hide();
-let json = api.boa.run('null-detector.boa');
-$('#loading').hide();
-$('#content').show();
-//console.log(json);
-let count = 0;
-for(let index in json.out["[]"]) {
-	count++;
-	let label = 
-		$('#table-output-body').append(`<tr><td> ${count} </td> <td> <i>${index}</i> </td> <td> ${json.out["[]"][index]} </td> </tr>`)
-}
+'use strict';
+$(window).load(function() {
+	let json = api.boa.run('null-detector.boa');
+	$('#loading').hide();
+	$('#content').show();
+	let count = 0;
+	let labels = [];
+	let dataset = [];
+	for(let index in json.out) {
+		count++;
+		let label =
+			$('#table-output-body').append(`<tr><td> ${count} </td> <td> ${index} </td> <td> ${json.out[index]} </td> </tr>`)
+			labels.push(index);
+		dataset.push(json.out[index]);
+	}
+	let chartData = {
+			labels: labels,
+			datasets: [{
+				fillColor: '#ff8080',
+				strokeColor: '#bf6060',
+				data: dataset
+			}]
+	}
 
+	let canvas = document.createElement('canvas');
+	canvas.setAttribute('width', '400px');
+	canvas.setAttribute('height', '300px');
+	canvas.id = "chart-output";
+	$('#content').prepend(canvas);
 
-
+	let ctx = canvas.getContext('2d');
+	new Chart(ctx).Bar(chartData, { 'responsive': true, });
+});
